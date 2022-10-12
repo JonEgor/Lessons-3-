238c4d2235272a5e72b27d9790c4391440de9064
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-12 22:16:22 +0300

    switch

--- Lessons-3-/Program.cs
+++ Lessons-3-/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace lessons
+{
+    /*
+       *Сalculator
+       */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondVale;
+                string action;
+
+                try
+                {
+                    Console.WriteLine("Enter number 1");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter number 2");
+                    secondVale = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Invalid format");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an operation '+' '-' '*' '/' ");
+                action = Console.ReadLine();
+                continue;
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondVale);
+                        break;
+                    case "-":
+                        Console.WriteLine(firstValue - secondVale);
+                        break;
+                    case "*":
+                        Console.WriteLine(firstValue * secondVale);
+                        break;
+                    case "/":
+                        if (secondVale == 0)
+                        {
+                            Console.WriteLine(0);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstValue / secondVale);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Error! Unknown action!");
+                        break;
+
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+/*
+ * Kalkulator
+ */
