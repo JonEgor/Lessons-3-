f0b70bf7b43e1069855ac10472f81758f4bd54de
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-12 22:18:41 +0300

    Else

--- Lessons-3-/Program.cs
+++ Lessons-3-/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace lessons
+{
+    /*
+       *Калькулятор
+       */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondVale;
+            string action;
+
+            Console.WriteLine("Введите число 1");
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+            secondVale = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию '+' '-' '*' '/' ");
+            action = Console.ReadLine();
+
+            if (action == "+")
+            {
+                Console.WriteLine(firstValue + secondVale);
+            }
+            else if (action == "-")
+            {
+                Console.WriteLine(firstValue - secondVale);
+            }
+            else if (action == "*")
+            {
+                Console.WriteLine(firstValue * secondVale);
+            }
+            else if (action == "/")
+            {
+                if (secondVale == 0)
+                    Console.WriteLine(0);
+                else
+                    Console.WriteLine(firstValue / secondVale);
+
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Неизветсное действие!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ * Kalkulator
+ */
